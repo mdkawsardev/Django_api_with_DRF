83b861012befc344eefaedbec9d0ee8a9c5636cc
mdkawsar <info.mdkawsardev@gmail.com>
2026-02-24 16:24:50 +0600

    I have created serializers.py, urls.py files and edited views.py fileJ

--- myapi/views.py
+++ myapi/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import *
+from .serializers import CommentSerializer
 # Create your views here.
+@api_view(['GET'])
+def comment_list(request):
+    all_commenst = Comments.objects.all()
+    serializer = CommentSerializer(all_commenst, many=True)
+    return Response(serializer.data)
