55887f3e5c8286323d4449d94522df3dacf37d1d
mdkawsar <info.mdkawsardev@gmail.com>
2026-02-24 18:57:56 +0600

    Finally, I made an API in GET and POST. I used Thunder client extenstion for testing API

--- myapi/views.py
+++ myapi/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import *
+from rest_framework import status
 from .serializers import CommentSerializer
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def comment_list(request):
-    all_commenst = Comments.objects.all()
-    serializer = CommentSerializer(all_commenst, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        all_commenst = Comments.objects.all()
+        serializer = CommentSerializer(all_commenst, many=True)
+        return Response({'status':200, 'all_data': serializer.data})
+
+
+    if request.method == "POST":
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
