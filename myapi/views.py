a84ffacfbce2d7509ad2f29bf2ff239fe3a56dcf
mdkawsar <info.mdkawsardev@gmail.com>
2026-03-09 20:51:30 +0600

    Removed unusable modules

--- myapi/views.py
+++ myapi/views.py
@@ -1,7 +1,5 @@
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
-from django.shortcuts import render
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import *
 from rest_framework import status
